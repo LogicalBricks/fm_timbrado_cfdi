0e35b40dadf2981ea74c337c1d6a56af14d245d2
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-12-24 12:15:06 -0600

    Primer refactor al parseo de la respuesta

--- lib/fm_timbrado_cfdi/fm_respuesta.rb
+++ lib/fm_timbrado_cfdi/fm_respuesta.rb
@@ -19,50 +19,9 @@ module FmTimbradoCfdi
         if savon_response.success? then
           #cargamos la respuesta en xml
           @doc = Nokogiri::XML(savon_response.to_xml)
-          #Parseamos el nodo xml
-          if not @doc.xpath("//xml").empty? then
-            @xml = Base64::decode64 @doc.xpath("//xml")[0].content
-            # tratamos de obtener el no de serie del CSD del emisor
-            begin
-              factura_xml = Nokogiri::XML(@xml)
-              @no_csd_emisor = factura_xml.xpath("//cfdi:Comprobante").attribute('noCertificado').value
-            rescue Exception => e
-              @no_csd_emisor = nil
-              @error = true
-              @errors << "No se ha podido obtener el CSD del emisor"
-            end
-          else
-            @xml = nil
-            @error = true
-            @errors << "No se ha encontrado el nodo xml"
-          end
-          #Parseamos el nodo timbre
-          if not @doc.xpath("//txt").empty? then
-            @timbre = FmTimbre.new Base64::decode64( @doc.xpath("//txt")[0].content )
-          else
-            @timbre = nil
-            #@error = true
-            @error = false
-            @errors << "No se ha encontrado el nodo para el timbre fiscal"
-          end
-          #Parseamos el nodo pdf
-          if not @doc.xpath("//pdf").empty? then
-            @pdf = Base64::decode64 @doc.xpath("//pdf")[0].content
-          else
-            @pdf = nil
-            #@error = true unless not @doc.xpath("//png").empty?
-            @error = false
-            @errors << "No se ha encontrado el nodo para la imagen cbb" unless not @doc.xpath("//png").empty?
-          end
-          #Parseamos el nodo cbb
-          if not @doc.xpath("//png").empty? then
-            @cbb = Base64::decode64 @doc.xpath("//png")[0].content
-          else
-            @cbb = nil
-            #@error = true unless not @doc.xpath("//pdf").empty?
-            @error = false
-            @errors << "No se ha encontrado el nodo para el archivo pdf" unless not @doc.xpath("//pdf").empty?
-          end
+          obtener_xml(@doc)
+          obtener_timbre(@doc)
+          obtener_pdf(@doc)
         else
           @error = true
           @errors << savon_response.soap_fault.to_s if savon_response.soap_fault.present?
@@ -101,6 +60,65 @@ module FmTimbradoCfdi
 
     def no_csd_emisor_present?
       not @no_csd_emisor.nil?
+    end
+
+    private
+
+    def obtener_xml(doc)
+      #Parseamos el nodo xml
+      if not doc.xpath("//xml").empty? then
+        @xml = Base64::decode64 doc.xpath("//xml")[0].content
+        # tratamos de obtener el no de serie del CSD del emisor
+        begin
+          factura_xml = Nokogiri::XML(@xml)
+          @no_csd_emisor = factura_xml.xpath("//cfdi:Comprobante").attribute('noCertificado').value
+        rescue Exception => e
+          @no_csd_emisor = nil
+          @error = true
+          @errors << "No se ha podido obtener el CSD del emisor"
+        end
+      else
+        @xml = nil
+        @error = true
+        @errors << "No se ha encontrado el nodo xml"
+      end
+    end
+
+    def obtener_timbre(doc)
+      #Parseamos el nodo timbre
+      if not doc.xpath("//txt").empty? then
+        @timbre = FmTimbre.new Base64::decode64( doc.xpath("//txt")[0].content )
+      else
+        @timbre = nil
+        #@error = true
+        @error = false
+        @errors << "No se ha encontrado el nodo para el timbre fiscal"
+      end
+
+    end
+
+    def obtener_pdf(doc)
+      #Parseamos el nodo pdf
+      if not doc.xpath("//pdf").empty? then
+        @pdf = Base64::decode64 doc.xpath("//pdf")[0].content
+      else
+        @pdf = nil
+        #@error = true unless not @doc.xpath("//png").empty?
+        @error = false
+        @errors << "No se ha encontrado el nodo para la imagen cbb" unless not @doc.xpath("//png").empty?
+      end
+    end
+
+    def obtener_cbb(doc)
+      #Parseamos el nodo cbb
+      if not doc.xpath("//png").empty? then
+        @cbb = Base64::decode64 doc.xpath("//png")[0].content
+      else
+        @cbb = nil
+        #@error = true unless not doc.xpath("//pdf").empty?
+        @error = false
+        @errors << "No se ha encontrado el nodo para el archivo pdf" unless not @doc.xpath("//pdf").empty?
+      end
     end
   end #class
 end #module
