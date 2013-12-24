ef4f2dd5a75b9e68ae56f9c9d4a19d93d3cc39be
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-12-24 13:43:44 -0600

    Refactorizando las pruebas de timbre para hacerlas más simples

--- spec/fm_timbrado_cfdi/fm_timbre_spec.rb
+++ spec/fm_timbrado_cfdi/fm_timbre_spec.rb
@@ -2,14 +2,14 @@
 require 'spec_helper'
 
 describe FmTimbradoCfdi::FmTimbre do
-  it "debe crear un objeto válido" do
-    timbre_as_text = File.open('spec/fixtures/timbre_example.txt').read
-    timbre = FmTimbradoCfdi::FmTimbre.new timbre_as_text 
+  context "debe crear un objeto válido" do
+    let(:timbre_as_text) { File.open('spec/fixtures/timbre_example.txt').read }
+    let(:timbre) { FmTimbradoCfdi::FmTimbre.new timbre_as_text }
     #Hay elementos que no deben estar vacíos
-    timbre.no_certificado_sat.should_not be_nil
-    timbre.fecha_timbrado.should_not be_nil
-    timbre.uuid.should_not be_nil
-    timbre.sello_sat.should_not be_nil
-    timbre.sello_cfd.should_not be_nil
+    it { timbre.no_certificado_sat.should_not be_nil }
+    it { timbre.fecha_timbrado.should_not be_nil }
+    it { timbre.uuid.should_not be_nil }
+    it { timbre.sello_sat.should_not be_nil }
+    it { timbre.sello_cfd.should_not be_nil }
   end
 end # describe FmTimbre
