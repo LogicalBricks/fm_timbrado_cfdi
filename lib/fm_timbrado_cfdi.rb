0fb0a06117f3c7a875e9eafb0d901f11c92840bb
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-12-26 11:36:21 -0600

    Agregando documentación a los métodos públicos principales

--- lib/fm_timbrado_cfdi.rb
+++ lib/fm_timbrado_cfdi.rb
@@ -14,10 +14,12 @@ module FmTimbradoCfdi
     @cliente ||= FmCliente.new
   end
 
-  #Envía un archivo xml al PAC para ser timbrado
-  #Regresa un objeto tipo PACResponse que contiene el xml certificado, el timbre y la representación en pdf o el cbb en png
-  # * <tt>xml</tt> - Es el archivo xml a ser timbrado como un string
-  # * <tt>generar_cbb</tt> - Es una bandera que indica si debe generarse el código cbb, si se genera el cbb no se genera el pdf, por default es false
+  # Public: Envía un archivo al PAC para ser timbrado en formato xml
+  #
+  # xml - Es el archivo XML sellado como un string
+  # generar_cbb - Es una bandera que indica si debe generarse el código cbb, por default es false
+  #
+  # Regresa un objeto tipo FMRespuesta que contiene el xml certificado, el timbre y la representación en pdf o el cbb en png
   def timbra_cfdi_xml (xml, generar_cbb = false)
     factura_xml = Nokogiri::XML(xml)
     #procesar rfc del emisor
@@ -26,16 +28,26 @@ module FmTimbradoCfdi
     respuesta = cliente.timbrar rfc, factura_xml.to_s, 'generarCBB' => generar_cbb
   end
 
-  #Envía un archivo al PAC para ser timbrado en formato layout
-  #Regresa un objeto tipo PACResponse que contiene el xml certificado, el timbre y la representación en pdf o el cbb en png
-  # * <tt>layout</tt> - Es el archivo layout a ser timbrado como un string
-  # * <tt>generar_cbb</tt> - Es una bandera que indica si debe generarse el código cbb, si se genera el cbb no se genera el pdf, por default es false
+  # Public: Envía un archivo al PAC para ser timbrado en formato layout
+  #
+  # rfc - Es el RFC del emisor
+  # layout - Es el archivo layout a ser timbrado como un string
+  # generar_cbb - Es una bandera que indica si debe generarse el código cbb, por default es false
+  #
+  # Regresa un objeto tipo FMRespuesta que contiene el xml certificado, el timbre y la representación en pdf o el cbb en png
   def timbra_cfdi_layout (rfc, layout, generar_cbb = false)
     respuesta = cliente.timbrar rfc, layout, 'generarCBB' => generar_cbb
   end
 
-  def timbrar (rfc, layout, opciones= {})
-    respuesta = cliente.timbrar rfc, layout, opciones
+  # Public: Envía un archivo al PAC para ser timbrado tanto en formato layout como en formato XML
+  #
+  # rfc - Es el RFC del emisor
+  # archivo - Es el archivo a ser timbrado como un string
+  # opciones - Es un hash de opciones que deben coincidir con los parámetros recibidos por Facturación Moderna para el timbrado
+  #
+  # Regresa un objeto tipo FMRespuesta que contiene el xml certificado, el timbre y la representación en pdf o el cbb en png
+  def timbrar (rfc, archivo, opciones= {})
+    respuesta = cliente.timbrar rfc, archivo, opciones
   end
 
   #no implementado todavía
