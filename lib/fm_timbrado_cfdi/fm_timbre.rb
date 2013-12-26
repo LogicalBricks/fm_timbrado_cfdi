826ea38813dd14cd3f72d52bea13400bfa520944
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-12-26 13:45:23 -0600

    Refactor de la obtención del timbre

--- lib/fm_timbrado_cfdi/fm_timbre.rb
+++ lib/fm_timbrado_cfdi/fm_timbre.rb
@@ -11,41 +11,31 @@ module FmTimbradoCfdi
 
     def parse ( nodo_timbre )
       valores = nodo_timbre.split("\n")
-      return unless valores.size == 9
-
-      # TransID
-      @trans_id = obtener_valor(valores[0])
-
-      # noCertificadoSAT
-      @no_certificado_sat = obtener_valor(valores[1])
-
-      # FechaTimbrado
-      @fecha_timbrado = obtener_valor(valores[2])
-
-      # uuid
-      @uuid = obtener_valor(valores[3])
-
-      # selloSAT
-      @sello_sat = obtener_valor(valores[4])
-
-      # selloCFD
-      @sello_cfd = obtener_valor(valores[5])
-
-      # Fecha
-      @fecha_comprobante = obtener_valor(valores[6])
-
-      # Serie
-      @serie = obtener_valor(valores[7])
-
-      # folio
-      @folio = obtener_valor(valores[8])
+      return nil unless valores.size == 9
+      hash = Hash[atributos.zip(valores)]
+      hash.each do |variable, valor|
+        instance_variable_set("@#{variable}", obtener_valor(valor))
+      end
     end
 
     private
-      def obtener_valor(cadena)
-        valores = cadena.chomp.split('|')
-        valores.size > 1 ? valores[1] : nil
-      end
+
+    def obtener_valor(cadena)
+      valores = cadena.chomp.split('|')
+      valores.size > 1 ? valores[1] : nil
+    end
+
+    def atributos
+      [ 'trans_id',
+        'no_certificado_sat',
+        'fecha_timbrado',
+        'uuid',
+        'sello_sat',
+        'sello_cfd',
+        'fecha_comprobante',
+        'serie',
+        'folio' ]
+    end
 
   end
 end
